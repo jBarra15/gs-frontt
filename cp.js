d7789706462983a8fdb0c3e7cffe6f8686ca862a
JBarra15 <joelbarrasokatu@hotmail.com>
2025-06-03 19:34:27 -0300

    mexendo em tudo dnv

--- cp.js
+++ cp.js
@@ -1,4 +1,4 @@
-// Exemplo de animação: mensagem de envio do formulário
+
 const form = document.getElementById('form-contato');
 const msg = document.getElementById('msg-form');
 
@@ -8,6 +8,3 @@ form.addEventListener('submit', function(event) {
     msg.style.color = '#1976d2';
     setTimeout(() => msg.textContent = '', 4000);
 });
-
-// Exemplo: placeholder para adicionar slider ou microinterações futuramente
-// function addSlider() { ... }
